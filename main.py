ed2ce951456f7ec771f7849fcc9712e8a5304c34
Ilya <st090885@student.spbu.ru>
2025-01-19 17:56:28 +0300

    Add statistics module and /stats command, change database schema and queries formatting for security, update the README

--- main.py
+++ main.py
@@ -19,38 +19,37 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         with conn.cursor() as cursor:
             # Check if the user already exists in db
-            cursor.execute(f"""
-                           SELECT user_id
-                           FROM user_words
-                           WHERE user_id = {user_id};
-                           """)
+            cursor.execute("""
+                           SELECT user_id 
+                           FROM users 
+                           WHERE user_id = %s;""", (user_id,))
+
             result = cursor.fetchone()
 
             if result:
                 # User exists
                 await update.message.reply_text(
-                    "*Welcome back\\!*\n"
+                    "*Welcome back\\!*\n\n"
                     "I will help you define and remember new vocabulary:\n\n"
                     "1\\. Type any word to get its definition\n"
                     "2\\. /mywords to see your wordlist\n\n"
                     "[Visit our bot's GitHub](https://github.com/IlMalakhov/vocab-bot)", 
-                    parse_mode="MarkdownV2"
-                    )
+                    parse_mode="MarkdownV2")
             else:
                 # User does not exist
-                cursor.execute("INSERT INTO user_words (user_id, words) VALUES (%s, %s);", (user_id, []))
+                cursor.execute("INSERT INTO users (user_id) VALUES (%s);", (user_id,))
                 conn.commit()
                 await update.message.reply_text(
-                    "Nice to meet you, I'm Vocab Bot\\!\n"                    
+                    "Nice to meet you, I'm Vocab Bot\\!\n\n"                    
                     "I will help you define and remember new vocabulary:\n\n"
                     "1\\. Type any word to get its definition\n"
                     "2\\. /mywords to see your wordlist\n\n"
                     "[Visit our bot's GitHub](https://github.com/IlMalakhov/vocab-bot)", 
                     parse_mode="MarkdownV2",
-                    disable_web_page_preview=True
-                    )
+                    disable_web_page_preview=True)
+
     except Exception as e:
-        await update.message.reply_text(f"An error occurred: {str(e)}")
+        print(f"Error fetching or inserting info about the user: {e}")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Type any word to get its definition.\nUse /mywords to see your saved words.')
@@ -81,18 +80,17 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if text:
         word = text.lower()
         definition = get_definition(word)
+
         if definition:
             # Create an inline button for adding the word
-            keyboard = [
-                [InlineKeyboardButton("Add Word", callback_data=f"add_{word}")]
-            ]
+            keyboard = [[InlineKeyboardButton("Add Word", callback_data=f"add_{word}")]]
             reply_markup = InlineKeyboardMarkup(keyboard)
 
             await update.message.reply_text(definition, reply_markup=reply_markup)
         else:
-            await update.message.reply_text("Definition not found.")
+            await update.message.reply_text("I coudnt't find the definition for that...")
     else:
-        await update.message.reply_text("Please enter a valid word.")
+        await update.message.reply_text("Something seems wrong with the word you sent me...")
 
 async def add_word_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     conn = context.bot_data["conn"]
@@ -101,32 +99,35 @@ async def add_word_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     # Extract the word from the callback data
     word = query.data.split('_', 1)[1]
+    word = word.strip().lower()
 
     try:
         with conn.cursor() as cursor:
-            # Check if the user already has an entry in the database
-            cursor.execute(f"""
-                           SELECT words 
+            # Ensure the word exists in the words table
+            cursor.execute("INSERT INTO words (word) VALUES (%s) ON CONFLICT (word) DO NOTHING;", (word,))
+
+            cursor.execute("SELECT word_id FROM words WHERE word = %s;", (word,))
+
+            word_id = cursor.fetchone()[0]
+
+            # Check if the user already has this word in their list
+            cursor.execute("""SELECT 1 
                            FROM user_words 
-                           WHERE user_id = {user_id};
-                           """)
+                           WHERE user_id = %s AND word_id = %s;""", (user_id, word_id))
+
             result = cursor.fetchone()
 
             if result:
-                words = result[0]
-                if word not in words:
-                    words.append(word)
-                    cursor.execute("UPDATE user_words SET words = %s WHERE user_id = %s;", (words, user_id))
-                    conn.commit()
-                    await query.answer(f'Added "{word}" to your word list.')
-                else:
-                    await query.answer(f'"{word}" is already in your word list.')
+                await query.answer(f'"{word}" is already in your word list.')
             else:
-                cursor.execute("INSERT INTO user_words (user_id, words) VALUES (%s, %s);", (user_id, [word]))
+                # Add the word to the user's word list
+                cursor.execute("""
+                               INSERT INTO user_words (user_id, word_id) 
+                               VALUES (%s, %s);""", (user_id, word_id))
                 conn.commit()
                 await query.answer(f'Added "{word}" to your word list.')
     except Exception as e:
-        await query.answer(f"An error occurred: {str(e)}")
+        await query.answer(f"Error trying to add a word to the list: {e}")
 
 async def mywords_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     conn = context.bot_data["conn"]
@@ -134,19 +135,38 @@ async def mywords_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     try:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT words FROM user_words WHERE user_id = %s;", (user_id,))
-            result = cursor.fetchone()
+            cursor.execute("""
+                           SELECT w.word 
+                           FROM user_words uw 
+                           JOIN words w 
+                           ON uw.word_id = w.word_id 
+                           WHERE uw.user_id = %s;""", (user_id,))
+            
+            results = cursor.fetchall()
 
-            if result and result[0]:
-                words = result[0]
+            if results:
+                words = [row[0] for row in results]
                 await update.message.reply_text("Your saved words:\n" + '\n'.join(words))
             else:
                 await update.message.reply_text("You have no saved words.")
     except Exception as e:
-        await update.message.reply_text(f"An error occurred: {str(e)}")
+        await update.message.reply_text(f"Error: {e}")
 
 async def stats_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    ...
+    conn = context.bot_data["conn"]
+    user_id = update.message.from_user.id
+
+    plot_png = stats_stuff.get_word_progress_plot(conn=conn, user_id=user_id)
+
+    # Send png
+    if plot_png:
+        await context.bot.send_photo(
+        chat_id=update.effective_chat.id,
+        photo=plot_png,
+        caption="Here is a graph of how many words you added over time\n\n*Great job\\!*", parse_mode="MarkdownV2"
+        )
+    else:
+        await update.message.reply_text("Coundn't find data for stats...")
 
 def main():
     application = Application.builder().token(TOKEN).build()
@@ -159,9 +179,9 @@ def main():
         application.add_handler(CommandHandler("start", start_command))
         application.add_handler(CommandHandler("help", help_command))
         application.add_handler(CommandHandler("mywords", mywords_command))
+        application.add_handler(CommandHandler("stats", stats_command))
         application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
         application.add_handler(CallbackQueryHandler(add_word_callback, pattern="^add_"))
-        application.add_handler(CommandHandler("stats", stats_command))
 
         print("Bot started")
         application.run_polling()
